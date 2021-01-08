76ef4cc09fa94b00288325767b59593f9b2fe3ab
YWSJpjOsDO <gOtQWGG4wZ>
2021-01-08 10:34:21 +0800

    这还是要提交所有文件才对啊

---
+++ src/main/java/com/eg/testsharding/TestShardingApplication.java
@@ -0,0 +1,13 @@
+package com.eg.testsharding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestShardingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestShardingApplication.class, args);
+    }
+
+}
