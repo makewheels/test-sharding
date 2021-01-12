3453279400a7440c0ed5ee446a6b35e83d358193
YWSJpjOsDO <gOtQWGG4wZ>
2021-01-12 15:19:34 +0800

    swagger为什么不能用

--- src/main/java/com/eg/testsharding/TestShardingApplication.java
+++ src/main/java/com/eg/testsharding/TestShardingApplication.java
@@ -2,7 +2,9 @@ package com.eg.testsharding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication
 public class TestShardingApplication {
 
--- src/main/java/com/eg/testsharding/controller/PoemController.java
+++ src/main/java/com/eg/testsharding/controller/PoemController.java
@@ -3,6 +3,8 @@ package com.eg.testsharding.controller;
 import com.eg.testsharding.bean.Poem;
 import com.eg.testsharding.bean.PoemExample;
 import com.eg.testsharding.bean.mapper.PoemMapper;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.annotation.Resource;
 import java.util.List;
 
+@Api("com.eg.testsharding.controller.PoemController")
 @Controller
 @RequestMapping("poem")
 public class PoemController {
@@ -18,6 +21,7 @@ public class PoemController {
     private PoemMapper poemMapper;
 
     /**
+     * id查询
      * http://localhost:8080/test-sharding/poem/getById?id=555697528192892928
      *
      * @param id
@@ -30,6 +34,7 @@ public class PoemController {
     }
 
     /**
+     * 搜索
      * http://localhost:8080/test-sharding/poem/queryByParagraphs?paragraphs=%E7%AE%80%E4%BA%BA
      *
      * @param paragraphs
@@ -39,8 +44,23 @@ public class PoemController {
     @ResponseBody
     public List<Poem> queryByParagraphs(@RequestParam String paragraphs) {
         PoemExample poemExample = new PoemExample();
-        poemExample.createCriteria().andParagraphsLike("%" + paragraphs + "%");
+        poemExample.createCriteria()
+                .andParagraphsLike("%" + paragraphs + "%");
+
         return poemMapper.selectByExample(poemExample);
+    }
+
+    /**
+     * count
+     * http://localhost:8080/test-sharding/poem/countPoems
+     *
+     * @return
+     */
+    @ApiOperation(value = "countPoems", notes = "notes")
+    @RequestMapping("countPoems")
+    @ResponseBody
+    public long countPoems() {
+        return poemMapper.countByExample(null);
     }
 
 }
